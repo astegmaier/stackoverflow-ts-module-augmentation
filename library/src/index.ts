a3c76503ce2f677b19a3707053940b6f81d8d323
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-11-15 13:42:59 -0500

    Real-life example.

--- library/src/index.ts
+++ library/src/index.ts
@@ -1,2 +1,18 @@
-export { Observable } from "./observable";
-import "./map";
+import { Components, createTheme, PaletteOptions } from '@mui/material/styles';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    ternary: Palette['primary'];
+    quaternary: Palette['primary'];
+    gridColor: string;
+    logoColor: string;
+  }
+  interface PaletteOptions {
+    ternary?: PaletteOptions['primary'];
+    quaternary?: PaletteOptions['primary'];
+    gridColor?: string;
+    logoColor?: string;
+  }
+}
+
+export const defaultTheme = createTheme();
--- library/src/observable.ts
+++
@@ -1,3 +0,0 @@
-export class Observable<T> {
-  // ... implementation left as an exercise for the reader ...
-}
